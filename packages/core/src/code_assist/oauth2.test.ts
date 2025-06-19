b71eab8957a9396e4b3f949b0b27e3266b364f3d
Scott Densmore <scottdensmore@mac.com>
2025-06-18 19:06:39 -0700

    Update to format file

--- packages/core/src/code_assist/oauth2.test.ts
+++ packages/core/src/code_assist/oauth2.test.ts
@@ -73,7 +73,6 @@ describe('oauth2', () => {
       (resolve) => (serverListeningCallback = resolve),
     );
 
-
     let capturedPort = 0;
     const mockHttpServer = {
       listen: vi.fn((port: number, callback?: () => void) => {
