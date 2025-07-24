d580cfd583303860db85b564dc159f41bee572eb
mkorwel <matt.korwel@gmail.com>
2025-07-24 16:58:22 -0700

    Revert "style: revert formatting changes to shell.ts"
    This reverts commit f91171534ce3ecb74c7f74c8609fe297330c551e.

--- packages/core/src/tools/shell.ts
+++ packages/core/src/tools/shell.ts
@@ -40,20 +40,7 @@ export class ShellTool extends BaseTool<ShellToolParams, ToolResult> {
     super(
       ShellTool.Name,
       'Shell',
-      `This tool executes a given shell command as `bash -c <command>`. Command can start background processes using `&`. Command is executed as a subprocess that leads its own process group. Command process group can be terminated as `kill -- -PGID` or signaled as `kill -s SIGNAL -- -PGID`.
-
-The following information is returned:
-
-Command: Executed command.
-Directory: Directory (relative to project root) where command was executed, or `(root)`.
-Stdout: Output on stdout stream. Can be `(empty)` or partial on error and for any unwaited background processes.
-Stderr: Output on stderr stream. Can be `(empty)` or partial on error and for any unwaited background processes.
-Error: Error or `(none)` if no error was reported for the subprocess.
-Exit Code: Exit Code or `(none)` if terminated by signal.
-Signal: Signal number or `(none)` if no signal was received.
-Background PIDs: List of background processes started or `(none)`.
-Process Group PGID: Process group started or `(none)`
-`,
+      `This tool executes a given shell command as \`bash -c <command>\`. Command can start background processes using \`&\`. Command is executed as a subprocess that leads its own process group. Command process group can be terminated as \`kill -- -PGID\` or signaled as \`kill -s SIGNAL -- -PGID\`.\n\nThe following information is returned:\n\nCommand: Executed command.\nDirectory: Directory (relative to project root) where command was executed, or \`(root)\` .\nStdout: Output on stdout stream. Can be \`(empty)\` or partial on error and for any unwaited background processes.\nStderr: Output on stderr stream. Can be \`(empty)\` or partial on error and for any unwaited background processes.\nError: Error or \`(none)\` if no error was reported for the subprocess.\nExit Code: Exit code or \`(none)\` if terminated by signal.\nSignal: Signal number or \`(none)\` if no signal was received.\nBackground PIDs: List of background processes started or \`(none)\` .\nProcess Group PGID: Process group started or \`(none)\` `,
       Icon.Terminal,
       {
         type: Type.OBJECT,
